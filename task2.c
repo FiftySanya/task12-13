64cfaaac9ee6819a4c98ebc8328f7cd6a859c71c
OLEKSANDR NUZHYN <Nuzhyn.Alex@LLL.kpi.ua>
2025-06-04 12:57:05 +0300

    Create task2.c

---
+++ task2.c
@@ -0,0 +1,13 @@
+#include <signal.h>
+#include <stdio.h>
+
+void handle_sigint(int sig) {
+    printf("Caught signal %d\n", sig);
+}
+
+int main() {
+    signal(SIGINT, handle_sigint);
+    while (1) {}
+        
+    return 0;
+}
